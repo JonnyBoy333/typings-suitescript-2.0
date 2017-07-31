b13e04d3d7d310b5bd3d6083c1959529839f9a4f
jonlamb <jonnylamb@gmail.com>
2017-07-31 10:40:13 -0500

    Updated certain Filter options to be optional

--- N/search.d.ts
+++ N/search.d.ts
@@ -3,13 +3,13 @@ export interface Filter {
     /** Name or internal ID of the search field as a string. */
     name: string;
     /** Join ID for the search filter as a string. */
-    join: string;
+    join?: string;
     /** Operator used for the search filter. See search.Operator. */
     operator: Operator;
     /** Summary type for the search filter. Use this property to get or set the value of the summary type. See search.Summary. */
-    summary: Summary;
+    summary?: Summary;
     /** Formula used by the search filter. Use this property to get or set the formula used by the search filter. */
-    formula: string;
+    formula?: string;
 }
 
 interface SearchColumnSetWhenOrderedByOptions {
